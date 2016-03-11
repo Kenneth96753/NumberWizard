fbc52e2f0d97c0790705de3f41ff425253db237f
Kenneth96753 <kenneth@purplemaia.org>
2016-03-11 11:15:07 -1000

    Commit of finished code

---
+++ Assets/levelmanager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelmanager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log ("Level load request for:  " + name);
+		Application.LoadLevel(name);
+	
+	}
+	public void QuitRequest(){
+		Debug.Log ("I want to quit.");
+		Application.Quit ();
+	}
+}
